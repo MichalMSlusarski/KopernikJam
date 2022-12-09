f29f03ebe18ab663010e1a9ba9380d8309718cbb
MichalSlusarski <mslusarski2@gmail.com>
2022-12-09 20:11:38 +0100

    dwu
    d

---
+++ KopernikGra/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] float zoomSpeed = 10;
+    [SerializeField] float maxZoomOut = 50;
+    [SerializeField] float mazZoomIn = 10;
+
+    void Update()
+    {
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        cam.orthographicSize -= scroll * zoomSpeed;
+    }
+}
