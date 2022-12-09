df17e4d973dd896d8a8d9a21669ac18bbf6bd5dd
MichalSlusarski <mslusarski2@gmail.com>
2022-12-09 22:19:47 +0100

    gy

---
+++ KopernikGra/Assets/Scripts/ArcadeRocket.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcadeRocket : MonoBehaviour
+{
+    // The speed at which the rocket will move
+    public float speed = 10.0f;
+
+    // The rate at which the rocket's thrust will regenerate
+    public float thrustRegenRate = 0.5f;
+
+    // The maximum amount of thrust that the rocket can have
+    public float maxThrust = 100.0f;
+
+    // The current amount of thrust that the rocket has
+    private float currentThrust = 0.0f;
+
+    // The rigidbody component of the rocket
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        // Get the rigidbody component of the rocket
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Check if the left mouse button is held down
+        if (Input.GetMouseButton(0))
+        {
+            // Check if the rocket has enough thrust to move
+            if (currentThrust > 0)
+            {
+                // Get the position of the mouse in world space
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                // Calculate the direction to move in
+                Vector2 direction = (Vector2)(mousePos - transform.position);
+                
+
+                // Normalize the direction so that the rocket moves at a constant speed
+                direction.Normalize();
+                
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+                // Rotate the rocket to face the direction of the mouse cursor
+                transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+                // Move the rocket in the specified direction
+                rb.velocity = direction * speed;
+
+                // Decrease the rocket's thrust
+                currentThrust -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            // If the left mouse button is not held down, regenerate the rocket's thrust
+            currentThrust += Time.deltaTime * thrustRegenRate;
+            currentThrust = Mathf.Clamp(currentThrust, 0, maxThrust);
+        }
+    }
+}
+
---
+++ KopernikGra/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody; // The player's rigidbody
+
+    public float moveSpeed = 5.0f; // The speed at which the player moves
+    public float jumpForce = 10.0f; // The force applied to the player when jumping
+
+    private bool isGrounded = false; // Whether the player is currently grounded
+    public float transformRadius = 0.1f; // The radius of the overlap circle to determine if the player is grounded
+    public LayerMask whatIsGround; // A mask determining what is ground to the player
+
+    void Update()
+    {
+        // Check if the player is grounded by performing a circle overlap test
+        isGrounded = Physics2D.OverlapCircle(transform.position, transformRadius, whatIsGround);
+
+        // Get the horizontal input axis
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Move the player horizontally
+        rigidbody.velocity = new Vector2(moveHorizontal * moveSpeed, rigidbody.velocity.y);
+
+        // If the player is grounded and the jump button is pressed, add a vertical force to the player
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
+
